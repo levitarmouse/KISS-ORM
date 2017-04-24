b0a1d703cc2b46237e5b2b8a8f655ae59a371a8f
gabriel <levitarmouse@gmail.com>
2017-04-24 13:39:06 -0300

    Pruebas integración !preAlpha

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,5 +2,6 @@
 mkdir -p config/kissorm;
 cp -rp ./vendor/levitarmouse/kiss_orm/tables.ini.dist ./tables.ini;
 ln -s ./vendor/levitarmouse/kiss_orm/kissGen.php .;
-cd ./config/kissorm/; ln -s ../../vendor/levitarmouse/kiss_orm/config/Bootstrap.php .;
-cd ./config/kissorm/; cp -rp ../../vendor/levitarmouse/kiss_orm/config/database.ini.dist ./database.ini;
+cd ./config/kissorm/;
+ln -s ../../vendor/levitarmouse/kiss_orm/config/Bootstrap.php .;
+cp -rp ../../vendor/levitarmouse/kiss_orm/config/database.ini.dist ./database.ini;
