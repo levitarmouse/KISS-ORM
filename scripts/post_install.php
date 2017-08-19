df639a54cfdefe036861db96ab718365c772cc39
gabriel <levitarmouse@gmail.com>
2017-08-19 09:35:23 -0300

    estabilizacion RC1

--- scripts/post_install.php
+++ scripts/post_install.php
@@ -2,10 +2,6 @@
 
 echo "KISS ORM POST INSTALL SCRIPT";
 
-$fwPath = __DIR__;
-
-//$ormPath   = __DIR__.'/vendor/levitarmouse/kiss_orm';
-
 $fwCfgPath = __DIR__.'/config';
 
 $ormCfgPath = __DIR__.'/config/kissorm';
@@ -18,10 +14,24 @@ if (!is_dir($fwCfgPath)) {
     mkdir($fwCfgPath);
 }
 
-mkdir($ormCfgPath);
+if (!is_dir($ormCfgPath)) {
+    mkdir($ormCfgPath);
+}
 
-copy('./vendor/levitarmouse/kiss_orm/tables.ini.dist', './tables.ini');
-copy('./vendor/levitarmouse/kiss_orm/config/database.ini.dist', './config/kissorm/database.ini');
+$modelsInfo = './tables.ini';
+$databaseCfg = './config/kissorm/database.ini';
+
+if (!file_exists($modelsInfo)) {
+    copy('./vendor/levitarmouse/kiss_orm/tables.ini.dist', $modelsInfo);
+} else {
+    echo 'INFO -> ALREADY EXIST ->'.$modelsInfo.PHP_EOL;
+}
+
+if (!file_exists($databaseCfg)) {
+    copy('./vendor/levitarmouse/kiss_orm/config/database.ini.dist', $databaseCfg);
+} else {
+    echo 'INFO -> ALREADY EXIST ->'.$databaseCfg.PHP_EOL;
+}
 
 symlink('./vendor/levitarmouse/kiss_orm/kissGen.php', './kissGen.php');
 
